0f4c3b34a0e585e1288123866fdc36e8284c917f
adriatoram <adriatoram@gmail.com>
2026-07-13 11:37:13 +0200

    test(destructure): Aprendre a utilitzar destructure, retorna undefined si no te el paramtre previ

--- exercises/04_destructure.test.js
+++ exercises/04_destructure.test.js
@@ -1,14 +1,14 @@
-const noop = () => {}
+const noop = () => { }
 
 const getAddress = () => ({
-    city: 'Salt Lake City',
-    state: 'UT',
-    zip: 84115,
-    coords: {
-      lat: 40.776608,
-      long: -111.920485,
-    },
-  })
+  city: 'Salt Lake City',
+  state: 'UT',
+  zip: 84115,
+  coords: {
+    lat: 40.776608,
+    long: -111.920485,
+  },
+})
 
 const getNumbers = () => [1, 2, 3, 4, 5, 6]
 
@@ -21,6 +21,8 @@ test('04_destructure-1: can be used to pull apart objects', () => {
   // const city = address.city;
   // const state = address.state;
   // const zip = address.zip;
+  const { city, state, zip } = getAddress();
+
   expect(city).toBe('Salt Lake City')
   expect(state).toBe('UT')
   expect(zip).toBe(84115)
@@ -29,12 +31,13 @@ test('04_destructure-1: can be used to pull apart objects', () => {
 test('04_destructure-2: sets missing values to undefined', () => {
   // Utilitzant desestructuració, intenta extreure una propietat anomenada `address` de l'objecte retornat per `getAddress()`.
   // Com que aquesta propietat no existeix, el seu valor serà `undefined`.
+  const { address } = getAddress();
   expect(address).toBeUndefined()
 })
 
 test('04_destructure-3: can be used to pull apart objects', () => {
   // Utilitzant desestructuració, crida `getAddress()` i crea les variables 'city', 'state' i 'zip'.
-  
+
   expect(city).toBe('Salt Lake City')
   expect(state).toBe('UT')
   expect(zip).toBe(84115)
@@ -48,7 +51,7 @@ test('04_destructure-4: sets missing values to undefined', () => {
 
 test('04_destructure-5: can alias destructured variables', () => {
   // Utilitzant desestructuració, crida `getAddress()` i extreu 'city', 'state' i 'zip', i assigna'ls els àlies c, s, z, respectivament.
-  
+
   expect(c).toBe('Salt Lake City')
   expect(s).toBe('UT')
   expect(z).toBe(84115)
@@ -59,7 +62,7 @@ test('04_destructure-5: can alias destructured variables', () => {
 
 test('04_destructure-6: can destructure nested variables', () => {
   // Utilitzant desestructuració, crida `getAddress()` i crea les variables `lat` i `long`.
-  
+
   expect(lat).toBe(40.776608)
   expect(long).toBe(-111.920485)
   expect(() => noop(coords)).toThrow()
@@ -67,14 +70,14 @@ test('04_destructure-6: can destructure nested variables', () => {
 
 test('04_destructure-7: can be used to pull apart arrays', () => {
   // Crida a getNumbers i extreu el primer valor com `one` i el segon com `two`.
-  
+
   expect(one).toBe(1)
   expect(two).toBe(2)
 })
 
 test('04_destructure-8: can skip indexes in arrays', () => {
   // Crida a getNumbers i extreu el primer valor com `one` i el tercer com `three`.
-  
+
   expect(one).toBe(1)
   expect(three).toBe(3)
   expect(() => noop(two)).toThrow()
@@ -82,7 +85,7 @@ test('04_destructure-8: can skip indexes in arrays', () => {
 
 test('04_destructure-9: can reach nested arrays', () => {
   // Crida a getNestedNumbers i extreu el primer valor com `one`, el 3 com `three` i el 6 com `six`.
-  
+
   expect(one).toBe(1)
   expect(three).toBe(3)
   expect(six).toBe(6)
