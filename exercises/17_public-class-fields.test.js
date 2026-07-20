1af2df01286592a4605b3845e579bb5333830983
adriatoram <adriatoram@gmail.com>
2026-07-20 19:37:27 +0200

    Feat(test): classes públiques amb funcions fletxa per evitar .bing

--- exercises/17_public-class-fields.test.js
+++ exercises/17_public-class-fields.test.js
@@ -1,8 +1,10 @@
+import { jest } from '@jest/globals';
+
 test('17_public-class-fields-1: public class fields help us avoid .bind-ing everything', () => {
   class FakeReactComponent {
     constructor(props) {
       this.props = props
-      this.setState = () => {} // només per diversió
+      this.setState = () => { } // només per diversió
     }
   }
 
@@ -10,10 +12,10 @@ test('17_public-class-fields-1: public class fields help us avoid .bind-ing ever
     constructor(...args) {
       super(...args)
       // no volem haver de fer això...
-      this.handleClick = this.handleClick.bind(this) // trist :-(
+      // this.handleClick = this.handleClick.bind(this) // trist :-(
     }
     // Converteix `handleClick` en un camp públic de classe utilitzant una funció fletxa (`=>`) perquè mantingui automàticament el valor correcte de `this` sense necessitat de fer `.bind(this)` al constructor.
-    handleClick({target: {value}}) {
+    handleClick = ({ target: { value } }) => {
       this.props.onClick(value)
     }
     render() {
@@ -21,13 +23,13 @@ test('17_public-class-fields-1: public class fields help us avoid .bind-ing ever
     }
     // això és només perquè puguem provar coses
     testClick(value) {
-      const fakeEvent = {target: {value}}
+      const fakeEvent = { target: { value } }
       this.handleClick(fakeEvent)
     }
   }
 
   const onClick = jest.fn()
-  const myComponent = new MyComponent({onClick})
+  const myComponent = new MyComponent({ onClick })
   myComponent.testClick('hello world')
   expect(onClick).toHaveBeenCalledTimes(1)
   expect(onClick).toHaveBeenCalledWith('hello world')
