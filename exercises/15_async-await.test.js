cf80b32738d53c80bc269bc0ac77d80069ac594e
adriatoram <adriatoram@gmail.com>
2026-07-20 19:28:38 +0200

    feat:(test): refactorització per fer servir async/await i try/caych

--- exercises/15_async-await.test.js
+++ exercises/15_async-await.test.js
@@ -2,25 +2,28 @@
 
 // Refactoritza'ls utilitzant `async/await` mantenint exactament el mateix comportament i fent que els tests continuïn passant.
 
-const doAsync = (rejectPromise = false) =>  new Promise((resolve, reject) => setTimeout(() => {
-      if (rejectPromise) {
-        reject('rejected')
-      } else {
-        resolve('resolved')
-      }
-    })
-  )
-
-  test('15_async-await-1: should work with resolved promises', async () => {
-  return doAsync().then(result => {
-    expect(result).toBe('resolved')
-  })
+const doAsync = (rejectPromise = false) => new Promise((resolve, reject) => setTimeout(() => {
+  if (rejectPromise) {
+    reject('rejected')
+  } else {
+    resolve('resolved')
+  }
 })
+)
+
+test('15_async-await-1: should work with resolved promises', async () => {
+  const result = await doAsync();
+  expect(result).toBe('resolved')
+})
+
 
 test('15_async-await-2: should throw an error with a rejected promise', async () => {
-  return doAsync(true).catch(error => {
+  try {
+    await doAsync(true);
+  } catch (error) {
     expect(error).toBe('rejected')
-  })
+  }
 })
+
 
 
